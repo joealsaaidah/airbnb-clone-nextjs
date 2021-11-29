77cf79bb7b9a47616eb2ebd29f3b90e6f04174b3
joealsaaidah <alsaaidahyousef@gmail.com>
2021-11-29 15:46:18 +0800

    set up files and create the header

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>MiMiCuCu-Airbnb</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      {/* Banner */}
+    </div>
+  );
+}
